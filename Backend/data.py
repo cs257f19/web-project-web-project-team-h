5e6cf40e605dcd5f3013873c937e21453d091ba4
qinemma <qine@carleton.edu>
2019-10-25 16:09:03 -0500

    Merging psycopg with data.py

--- Backend/data.py
+++ Backend/data.py
@@ -103,3 +103,43 @@ class Data:
         except Exception as e:
     		print ("Something went wrong when executing the query: ", e)
     		return None
+
+def connect(user, password):
+	'''
+	Establishes a connection to the database with the following credentials:
+		user - username, which is also the name of the database
+		password - the password for this database on perlman
+
+	Returns: a database connection.
+
+	Note: exits if a connection cannot be established.
+	'''
+	try:
+		connection = psycopg2.connect(database=user, user=user, password=password)
+	except Exception as e:
+		print("Connection error: ", e)
+		exit()
+	return connection
+
+
+def main():
+
+    user = "qine"
+    password = getpass.getpass()
+
+    # Connect to the database
+    connection = connect(user, password)
+
+	# Execute a simple query: how many earthquakes above the specified magnitude are there in the data?
+    query = Data()
+    results = query.getHostInfo(connection, 2787)
+
+    if results is not None:
+		print("Query results: ")
+		for item in results:
+			print(item)
+
+	# Disconnect from database
+    connection.close()
+
+main()
